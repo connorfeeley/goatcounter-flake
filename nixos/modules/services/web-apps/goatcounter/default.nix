7c07d178a0178b2f75a0c3ce40e75949c9598fe4
Connor Feeley <git@cfeeley.org>
2023-11-17 09:00:00 -0500

    module: fix systemd permissions

--- nixos/modules/services/web-apps/goatcounter/default.nix
+++ nixos/modules/services/web-apps/goatcounter/default.nix
@@ -12,12 +12,11 @@ let
 
   cfg = config.services.goatcounter;
 
-  goatcounterArgs = lib.concatStringsSep " " ([
-    cfg.database.backend
-    "dbname=${cfg.database.name}"
-    (lib.optionalString (cfg.database.user != null) "user=${cfg.database.user}")
-    (lib.optionalString (cfg.database.passwordFile != null) "passfile=${cfg.database.passwordFile}")
-  ] ++ cfg.database.extraArgs);
+  goatcounterEnv = ([
+    "PGDATABASE=${cfg.database.name}"
+    (lib.optionalString (cfg.database.user != null) "PGUSER=${cfg.database.user}")
+    (lib.optionalString (cfg.database.passwordFile != null) "PGPASSFILE=%d/passwordFile")
+  ]);
 in
 {
   options.services.goatcounter = {
@@ -43,15 +42,12 @@ in
           Path to a file containing the password for the database user.
 
           Should contain lines of the following format:
-
           hostname:port:database:username:password
+
+          Must have permissions 0600 or less to be read by the user running goatcounter.
+
           See https://www.postgresql.org/docs/current/libpq-pgpass.html for more information.
         '';
-      };
-      extraArgs = mkOption {
-        type = types.listOf types.str;
-        default = [ ];
-        example = [ "sslmode=disable" ];
       };
     };
     environmentFile = mkOption {
@@ -68,6 +64,11 @@ in
         `goatcounter` is running.
       '';
     };
+    extraArgs = mkOption {
+      type = types.listOf types.str;
+      default = [ ];
+      example = [ "-listen='*:8002'" "-tls=none" "-debug=all" ];
+    };
   };
 
   config = mkIf cfg.enable {
@@ -76,7 +77,10 @@ in
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
         DynamicUser = "yes";
-        ExecStart = "${cfg.package}/bin/goatcounter serve -db '${goatcounterArgs}'";
+        Environment = goatcounterEnv;
+        EnvironmentFile = [ ] ++ lib.optional (cfg.environmentFile != null) cfg.environmentFile;
+        LoadCredential = lib.mkIf (cfg.database.passwordFile != null) "passwordFile:${cfg.database.passwordFile}";
+        ExecStart = "${cfg.package}/bin/goatcounter serve -db '${cfg.database.backend}' ${lib.concatStringsSep " " cfg.extraArgs}";
         Restart = "on-failure";
         RestartSec = "5s";
       };
