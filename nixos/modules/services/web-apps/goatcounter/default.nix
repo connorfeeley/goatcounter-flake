00080755fe0f292b80dbdf3f108645fd0537f2b1
Connor Feeley <git@cfeeley.org>
2023-11-18 09:00:00 -0500

    module: add automigrate flag

--- nixos/modules/services/web-apps/goatcounter/default.nix
+++ nixos/modules/services/web-apps/goatcounter/default.nix
@@ -20,41 +20,11 @@ let
 in
 {
   options.services.goatcounter = {
-    enable = mkEnableOption (lib.mdDoc "Enable the goatcounter service.");
+    enable = mkEnableOption (lib.mdDoc "enable the goatcounter service");
     package = mkOption {
-      defaultText = lib.literalMD "`packages.default` from the foo flake";
-      description = lib.mdDoc ''
-        Extra arguments to be passed to `goatcounter serve`.
-      '';
-    };
-
-
-    # TODO: seperate sqlite and postgres options.
-    database = {
-      backend = mkOption {
-        type = types.enum [ "postgresql" "sqlite" ];
-        default = lib.literalMD "postgresql";
-        description = lib.mdDoc "Databse backend.";
-      };
-
-      # host = mkOption { type = types.str; description = "Database host."; };
-      name = mkOption { type = types.str; description = "Database to connect to."; };
-      user = mkOption { type = types.str; description = "Postgresql user."; };
-      passwordFile = mkOption {
-        type = with types; nullOr path;
-        default = null;
-        example = lib.literalMD "/var/lib/goatcounter.passwd";
-        description = lib.mdDoc ''
-          Path to a file containing the password for the database user.
-
-          Should contain lines of the following format:
-          hostname:port:database:username:password
-
-          Must have permissions 0600 or less to be read by the user running goatcounter.
-
-          See https://www.postgresql.org/docs/current/libpq-pgpass.html for more information.
-        '';
-      };
+      defaultText = lib.literalMD "`packages.default` from the `goatcounter` flake";
+      description = lib.mdDoc "The goatcounter package to use.";
+      type = types.package;
     };
     environmentFile = mkOption {
       type = with types; nullOr path;
@@ -63,8 +33,8 @@ in
       description = lib.mdDoc ''
         Additional environment file as defined in {manpage}`systemd.exec(5)`.
 
-        Secrets like {env}`PGDATABASE` and {env}`DBHOST`
-        may be passed to the service without adding them to the world-readable Nix store.
+        Secrets like {env}`PGDATABASE` and {env}`DBHOST` may be passed to the
+        service without adding them to the world-readable Nix store.
 
         Note that this file needs to be available on the host on which
         `goatcounter` is running.
@@ -73,10 +43,48 @@ in
     extraArgs = mkOption {
       type = types.listOf types.str;
       default = [ ];
-      example = [ "-listen='*:8002'" "-tls=none" "-debug=all" ];
+      example = [ "-listen='*:8002'" "-tls=http" "-debug=all" ];
       description = lib.mdDoc ''
-        Extra arguments to be passed to `goatcounter serve`.
+        Extra command-line arguments to be passed to `goatcounter serve`.
+
+        By default, GoatCounter listens on port 80 and 443 and tries to generate
+        an ACME/Let's Encrypt certificate.
+
+        To run GoatCounter behind a proxy like Nginx, you can change the port
+        with `-listen='*:8002'` and set -tls=http to disable certificate generation.
       '';
+    };
+
+    # TODO: seperate sqlite and postgres options.
+    database = {
+      backend = mkOption {
+        type = types.enum [ "postgresql" "sqlite" ];
+        default = lib.literalMD "postgresql";
+        description = lib.mdDoc "Database backend to use.";
+      };
+
+      name = mkOption { type = types.str; description = "Database name to connect to."; };
+      user = mkOption { type = types.str; description = "PostgreSQL user."; };
+      automigrate = mkEnableOption (lib.mdDoc "automatically migrate the database schema");
+      passwordFile = mkOption {
+        type = with types; nullOr path;
+        default = null;
+        example = lib.literalMD "/var/lib/goatcounter.passwd";
+        description = lib.mdDoc ''
+          Path to a file containing the password for the database user.
+
+          The service will use use `LoadCredential` to expose the file to the service using {env}`PGPASSFILE`.
+
+          Should contain lines of the following format:
+          ```
+          hostname:port:database:username:password
+          ```
+
+          Must have permissions 0600 or less to be read by the user running goatcounter.
+
+          See [PostgreSQL: Documentation: 16: 34.16. The Password File](https://www.postgresql.org/docs/current/libpq-pgpass.html) for more information.
+        '';
+      };
     };
   };
 
@@ -86,14 +94,20 @@ in
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
         DynamicUser = "yes";
+        LoadCredential = lib.mkIf (cfg.database.passwordFile != null) "passwordFile:${cfg.database.passwordFile}";
         Environment = [
           "PGDATABASE=${cfg.database.name}"
           (lib.optionalString (cfg.database.user != null) "PGUSER=${cfg.database.user}")
           (lib.optionalString (cfg.database.passwordFile != null) "PGPASSFILE=%d/passwordFile")
         ];
-        EnvironmentFile = [ ] ++ lib.optional (cfg.environmentFile != null) cfg.environmentFile;
-        LoadCredential = lib.mkIf (cfg.database.passwordFile != null) "passwordFile:${cfg.database.passwordFile}";
-        ExecStart = "${cfg.package}/bin/goatcounter serve -db '${cfg.database.backend}' ${lib.concatStringsSep " " cfg.extraArgs}";
+        EnvironmentFile = lib.optionals (cfg.environmentFile != null) [ cfg.environmentFile ];
+        ExecStart = lib.concatStringsSep " " [
+          "${cfg.package}/bin/goatcounter"
+          "serve"
+          "-db '${cfg.database.backend}'"
+          (lib.optionalString cfg.database.automigrate "-automigrate")
+          (lib.concatStringsSep " " cfg.extraArgs)
+        ];
         Restart = "on-failure";
         RestartSec = "5s";
       };
